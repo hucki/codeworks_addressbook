18efca574ee3b5e03b465c7a8773e44272be78c2
Hucki <satanklaus@gmail.com>
2020-02-10 19:31:09 +0100

    integrated form "new Contact" into table. moved form tags around table removed form tags altogether removed button "add new contact" prepared delete function via checkbox

--- js/script.js
+++ js/script.js
@@ -5,34 +5,18 @@ class AddressEntry {
     this._phone = phone;
     this._address = address;
   }
-  get name() {
-    return this._name;
-  }
-  set name(name) {
-    this._name = name;
-  }
-  get surname() {
-    return this._surname;
-  }
-  set surname(surname) {
-    this._surname = surname;
-  }
-  get phone() {
-    return this._phone;
-  }
-  set phone(phone) {
-    this._phone = phone;
-  }
-  get address() {
-    return this._address;
-  }
-  set address(address) {
-    this._address = address;
-  }
+  get name()            {return this._name;}
+  set name(name)        {this._name = name;}
+  get surname()         {return this._surname;}
+  set surname(surname)  {this._surname = surname;}
+  get phone()           {return this._phone;}
+  set phone(phone)      {this._phone = phone;}
+  get address()         {return this._address;}
+  set address(address)  {this._address = address;}
 };
 
-let numberOfContacts = 0;
 let addressBook = [];
+let numberOfContacts = addressBook.length;
 let contactsTable;
 let contactsTableHtml;
 
@@ -40,23 +24,16 @@ function showContactList() {
   numberOfContacts = addressBook.length;
   if(numberOfContacts > 0) {
     $('#contacts_table').text('');
-
-    $('<tr>')
-      .appendTo($('<thead>')
-        .appendTo($('<table>')
-          .addClass('actualContactList')
-          .appendTo('#contacts_table')));
-    for (let [key, value] of Object.entries(addressBook[0])) {
-      $('<td>').text(`${key}`.substring(1,key.length)).appendTo('tr');
-    }
+    $('#actualContactList tbody').html('');
     for(let i = 0; i < numberOfContacts; i++) {
-      (i===0?$('<tbody>').appendTo('.actualContactList'):null);
-      $('<tr>').addClass((i%2 === 0 ? 'even' : 'odd')).attr('id','contact_'+i).appendTo('.actualContactList tbody');
+      //(i===0?$('<tbody>').appendTo('.actualContactList'):null);
+      $('<tr>').addClass((i%2 === 0 ? 'even' : 'odd')).attr('id','contact_'+i).appendTo('#actualContactList tbody');
+      $('<td>').addClass('checker').html(`<input id="check_${i}" type="checkbox" value="${i}" name="check_${i}">`).appendTo('#contact_'+i);
       for (let [key, value] of Object.entries(addressBook[i])) {
         $('<td>').text(`${value}`).appendTo('#contact_'+i);
       }
+      //$('<td>').html(`<button id="delete_${i}" class="deleteX">x</button>`).appendTo('#contact_'+i);
     }
-
   } else {
     $('#contacts_table').text('no Contacts found!');
   }
@@ -66,7 +43,7 @@ function showContactList() {
 function addAddress(name,surname,phone,address) {
   let addressId = addressBook.length;
   addressBook[addressId] = new AddressEntry(name,surname,phone,address);
-  $('#newAddressForm').find('input[type=text]').val('');
+  $('#actualContactList').find('input[type=text]').val('');
   showContactList();
 }
 
@@ -75,35 +52,37 @@ function deleteAddress(id) {
 }
 
 function liveSearch() {
-  let searchValue = $(this).val().toLowerCase();
-  if(searchValue != '') {
-    $('.actualContactList tbody tr td').each(function(){
-      let findInText = $(this).text().toLowerCase();
-      if (findInText.indexOf(searchValue) >= 0) {
-        let foundValue = $(this).text().substr(findInText.indexOf(searchValue), searchValue.length);
-        let highlightText = $(this).text();
-        $(this).html(highlightText.replace(foundValue, `<span class="found">${foundValue}</span>`));
-      } else {
-        $(this).html($(this).text());
-      }
-    })
+  let foundContacts = [];
+  let searchValue = $(this).val().toLowerCase(); //.split(' ');
+  console.log(searchValue);
+
+  if(searchValue.length > 0) {
+    //searchValue.forEach((item, i) => {
+      $('#actualContactList tbody tr td').not('.checker').each(function(){
+        let findInText = $(this).text().toLowerCase();
+        if (findInText.indexOf(searchValue) >= 0) {
+          (foundContacts.findIndex((element) => element === $(this).parent().attr('id')) < 0) ? foundContacts.push($(this).parent().attr('id')) : null;
+          let foundValue = $(this).text().substr(findInText.indexOf(searchValue), searchValue.length);
+          let highlightText = $(this).text();
+          $(this).html(highlightText.replace(foundValue, `<span class="found">${foundValue}</span>`));
+          $(this).parent().show();
+        } else {
+          $(this).html($(this).text());
+          (foundContacts.findIndex((element) => element === $(this).parent().attr('id')) < 0) ? $(this).parent().hide() : null;
+        }
+      })
+    //});
   } else {
-    $('.actualContactList tbody tr td').each(function(){
+    $('#actualContactList tbody tr td').not('.checker').each(function(){
       $(this).html($(this).text());
+      $(this).parent().show();
     });
   }
-console.log(searchValue);
 }
 
-function showNewAddressForm() {
-  $('#newAddressForm').slideDown();
-}
-function hideNewAddressForm() {
-  $('#newAddressForm').slideUp();
-}
 function resetContacts() {
   addressBook = [];
-  addAddress('John','Doe', '+34 601 465 366', 'Friedrichstr. 76, 10117 Berlin', );
+  addAddress('John','Doe', '+34 601 465 366', 'Friedrichstr. 76, 10117 Berlin');
   addAddress('Jane','Smith', '+34 601 465 366', `Carrer d'Àvila, 27, 08005 Barcelona`);
   addAddress('Max','Mad');
 
