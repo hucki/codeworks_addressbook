c8b19f5a81c15ab8b3703355fb1a7cde9e659e6e
Hucki <satanklaus@gmail.com>
2020-02-12 16:30:25 +0100

    added more dummy addresses added navigaton buttons responsive optimization

--- js/script.js
+++ js/script.js
@@ -18,21 +18,22 @@ class AddressEntry {
 let addressBook = [];
 let addressBookDeleted = [];
 let addressBookDeletedArray = [];
+const maxPerPage = 25;
+let pageNr = 1;
 let numberOfContacts = addressBook.length;
+let numberOfContactsPage = 25;
 let contactsTable;
 let contactsTableHtml;
 
 function showContactList() {
+  $('.controlsRight').find('button').prop( "disabled", true );
   numberOfContacts = addressBook.length;
+  numberOfContactsPage = maxPerPage*pageNr > numberOfContacts ? numberOfContacts : maxPerPage*pageNr;
   if(numberOfContacts > 0) {
     $('#contacts_table').text('');
     $('#actualContactList tbody').html('');
-    for(let i = 0; i < numberOfContacts; i++) {
-      $('<tr>').addClass((i%2 === 0 ? 'even' : 'odd')).attr('id','contact_'+i).appendTo('#actualContactList tbody');
-      $('<td>').addClass('checker').html(`<input id="check_${i}" type="checkbox" value="${i}" name="check_${i}">`).appendTo('#contact_'+i);
-      for (let [key, value] of Object.entries(addressBook[i])) {
-        $('<td>').text(`${value}`).appendTo('#contact_'+i);
-      }
+    for(let i = 0; i < numberOfContactsPage; i++) {
+      addTableRow(i);
     }
   } else {
     $('#contacts_table').text('no Contacts found!');
@@ -41,18 +42,35 @@ function showContactList() {
 
 }
 
+function addTableRow(addressId) {
+  $('<tr>').addClass((addressId%2 === 0 ? 'even' : 'odd')).attr('id','contact_'+addressId).appendTo('#actualContactList tbody');
+  $('<td>').addClass('checker').html(`<input id="check_${addressId}" type="checkbox" value="${addressId}" name="check_${addressId}">`).appendTo('#contact_'+addressId);
+  for (let [key, value] of Object.entries(addressBook[addressId])) {
+    $('<td>').text(`${value}`).appendTo('#contact_'+addressId);
+  }
+}
+
 function addAddress(name,surname,phone,address) {
   let addressId = addressBook.length;
   addressBook[addressId] = new AddressEntry(name,surname,phone,address);
+  addTableRow(addressId);
   $('#actualContactList').find('input[type=text]').val('');
-  showContactList();
 }
 
+function showContactInput(event){
+  $('#actualContactList').find('input[type=text]').show();
+/*
+  $('table :input[type=text]').each(function() {
+    $(this).slideDown();
+  })
+*/
+}
 function addContact(event) {
   event.preventDefault();
   if ( (event.type === 'keyup' && event.which === 13) || event.type === 'click' ) {
     if($('#newFirstName').val()||$('#newSurname').val()||$('#newPhone').val()||$('#newAddress').val()) {
       addAddress($('#newFirstName').val(),$('#newSurname').val(),$('#newPhone').val(),$('#newAddress').val());
+      $('#actualContactList').find('input[type=text]').hide();
     } else {
       alert('Please input data in at least one given field.');
     }
@@ -60,7 +78,6 @@ function addContact(event) {
 }
 
 function deleteContacts(ids) {
-
   addressBook.splice(id);
   showContactList();
 }
@@ -72,21 +89,19 @@ function liveSearch() {
   console.log(searchValue);
 
   if(searchValue.length > 0) {
-    //searchValue.forEach((item, i) => {
-      $('#actualContactList tbody tr td').not('.checker').each(function(){
-        let findInText = $(this).text().toLowerCase();
-        if (findInText.indexOf(searchValue) >= 0) {
-          (foundContacts.findIndex((element) => element === $(this).parent().attr('id')) < 0) ? foundContacts.push($(this).parent().attr('id')) : null;
-          let foundValue = $(this).text().substr(findInText.indexOf(searchValue), searchValue.length);
-          let highlightText = $(this).text();
-          $(this).html(highlightText.replace(foundValue, `<span class="found">${foundValue}</span>`));
-          $(this).parent().show();
-        } else {
-          $(this).html($(this).text());
-          (foundContacts.findIndex((element) => element === $(this).parent().attr('id')) < 0) ? $(this).parent().hide() : null;
-        }
-      })
-    //});
+    $('#actualContactList tbody tr td').not('.checker').each(function(){
+      let findInText = $(this).text().toLowerCase();
+      if (findInText.indexOf(searchValue) >= 0) {
+        (foundContacts.findIndex((element) => element === $(this).parent().attr('id')) < 0) ? foundContacts.push($(this).parent().attr('id')) : null;
+        let foundValue = $(this).text().substr(findInText.indexOf(searchValue), searchValue.length);
+        let highlightText = $(this).text();
+        $(this).html(highlightText.replace(foundValue, `<span class="found">${foundValue}</span>`));
+        $(this).parent().show();
+      } else {
+        $(this).html($(this).text());
+        (foundContacts.findIndex((element) => element === $(this).parent().attr('id')) < 0) ? $(this).parent().hide() : null;
+      }
+    })
   } else {
     $('#actualContactList tbody tr td').not('.checker').each(function(){
       $(this).html($(this).text());
@@ -97,9 +112,38 @@ function liveSearch() {
 
 function resetContacts() {
   addressBook = [];
-  addAddress('John','Doe', '+34 601 465 366', 'Friedrichstr. 76, 10117 Berlin');
-  addAddress('Jane','Smith', '+34 601 465 366', `Carrer d'Àvila, 27, 08005 Barcelona`);
-  addAddress('Max','Mad');
+  addAddress('Gabi','Müller', '+49 30 465 366', 'Friedrichstr. 76, 10117 Berlin');
+  addAddress('John','Doe', '+34 601 465 366', `Carrer d'Àvila, 27, 08005 Barcelona`);
+  addAddress('Max','Mad','','The Thunderdome, Down Under');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('Jack','Mayne','+11 234 567 89','Los Angeles');
+  addAddress('Jill','Payne','+11 234 567 89','Los Angeles');
+  addAddress('Peter','Cayne','+11 234 567 89','Los Angeles');
+  addAddress('Paul','Dayne','+11 234 567 89','Los Angeles');
+  addAddress('Mary','Layne','+11 234 567 89','Los Angeles');
+  addAddress('Jimbo','Jayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Bayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Fayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
+  addAddress('John','Wayne','+11 234 567 89','Los Angeles');
 
   showContactList();
 }
